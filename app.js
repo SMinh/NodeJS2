1b6c60649671cfa51ad2da95656a63acdeb6b8b1
Son Minh <minh.huynhcao@gmail.com>
2022-07-14 18:42:10 +0700

    lab2.4

--- app.js
+++ app.js
@@ -4,16 +4,19 @@ const express = require("express");
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("In the middleware!");
+app.use("/", (req, res, next) => {
+  console.log("This always runs!");
   next();
 });
 
-app.use((req, res, next) => {
+app.use("/add-product", (req, res, next) => {
+  console.log("In the another middleware!");
+  res.send("<h1>The Add product page</h1>");
+});
+
+app.use("/", (req, res, next) => {
   console.log("In the another middleware!");
   res.send("<h1>Hello from Express!</h1>");
 });
 
-const server = http.createServer(app);
-
-server.listen(3000);
+app.listen(3000);
