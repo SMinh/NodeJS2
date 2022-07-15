43d2bdac9aa7b7ee49f04e6830a829973168168d
Son Minh <minh.huynhcao@gmail.com>
2022-07-15 07:43:31 +0700

    lab2.6

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
-const http = require("http");
+//const http = require("http");
 
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use("/", (req, res, next) => {
-  console.log("This always runs!");
-  next();
-});
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use("/add-product", (req, res, next) => {
-  console.log("In the another middleware!");
-  res.send("<h1>The Add product page</h1>");
+  res.send(
+    "<form action='/product' method='POST'><input type='text' name='title'><button type='submit'>Add product</button></input></form>"
+  );
+});
+
+app.post("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
 });
 
 app.use("/", (req, res, next) => {
